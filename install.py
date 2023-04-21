e235b9a29d0cda23d9b0e4c69366fd32c65ac4ea
firstvanq <76738608+firstvanq@users.noreply.github.com>
2023-04-21 22:53:59 +0530

    gdown for models

--- install.py
+++ install.py
@@ -10,3 +10,9 @@ try:
     os.remove(os.path.join(path, "pixelization", "models", "__init__.py"))
 except OSError as e:
     pass
+#install older gdown version to avoid download error:
+!pip install gdown==4.5.4 --no-cache-dir
+#download the shared files by copypasting the file identifier that comes after "https://drive.google.com/file/d/" into "https://drive.google.com/uc?id="
+!gdown https://drive.google.com/uc?id=1VRYKQOsNlE1w1LXje3yTRU5THN2MGdMM
+!gdown https://drive.google.com/uc?id=17f2rKnZOpnO9ATwRXgqLz5u5AZsyDvq_
+!gdown https://drive.google.com/uc?id=1i_8xL3stbLWNF4kdQJ50ZhnRFhSDh3Az
